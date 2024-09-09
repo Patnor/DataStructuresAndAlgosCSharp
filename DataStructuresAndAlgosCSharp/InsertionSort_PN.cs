43a8da5dfde26197e0fb040a455f1ee18775f291
Patnor <patnor22@gmail.com>
2024-09-08 20:18:38 -0700

    created insertion sort for array of int

---
+++ DataStructuresAndAlgosCSharp/InsertionSort_PN.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgosCSharp
+{
+    internal class InsertionSort_PN
+    {
+        /// <summary>
+        /// Sorts an array of integers using the insertion sort algorithm.
+        /// </summary>
+        /// <param name="nums">The array of integers to be sorted.</param>
+        public static void InsertionSort(int[] nums)
+        {
+            // Iterate over each element in the array starting from the second element
+            for (int i = 1; i < nums.Length; i++)
+            {
+                // Store the current element and the index of the previous element
+                int j = i - 1;
+                int temp = nums[i];
+
+                // Shift elements of the sorted segment forward if they are greater than the current element
+                while (j > -1 && nums[j] > temp)
+                {
+                    nums[j + 1] = nums[j];
+                    j--;
+                }
+                // Place the current element in its correct position
+                nums[j + 1] = temp;
+            }
+        }
+    }
+}
--- DataStructuresAndAlgosCSharp/Program.cs
+++ DataStructuresAndAlgosCSharp/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Test array
+            int[] testArray = { 5, 2, 9, 1, 5, 6 };
+
+            // Print the original array
+            Console.WriteLine("Original array:");
+            PrintArray(testArray);
+
+            // Sort the array using InsertionSort
+            InsertionSort_PN.InsertionSort(testArray);
+
+            // Print the sorted array
+            Console.WriteLine("Sorted array:");
+            PrintArray(testArray);
+        }
+
+        // Helper method to print the array
+        static void PrintArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
         }
     }
 }

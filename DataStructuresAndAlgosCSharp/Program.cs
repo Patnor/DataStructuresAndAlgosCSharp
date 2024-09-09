b6efc872d47ca1f101a432651d48c0e80ac93d74
Patnor <patnor22@gmail.com>
2024-09-08 19:17:44 -0700

    Add project files.

---
+++ DataStructuresAndAlgosCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructuresAndAlgosCSharp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

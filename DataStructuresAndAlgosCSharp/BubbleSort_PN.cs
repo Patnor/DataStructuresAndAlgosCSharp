a8873639f4c4513556a018942ecb7464124903cd
Patnor <patnor22@gmail.com>
2024-09-26 13:39:14 -0700

    Finished bubble sort

--- DataStructuresAndAlgosCSharp/BubbleSort_PN.cs
+++ DataStructuresAndAlgosCSharp/BubbleSort_PN.cs
@@ -4,10 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataStructuresAndAlgosCSharp
-{
+namespace DataStructuresAndAlgosCSharp { 
+
+
+    // each pass moves the largest remaining elements
+    // to the end of the unsorted elements
+    // O(n^2) time complexity
+    // bubble sort is stable.
     public class BubbleSort_PN
     {
+        public static void BubbleSort(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                for(int j = i; j < array.Length -1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // swap
+                        //int temp = array[i];
+                        //array[i] = array[i + 1];
+                        //array[i + 1] = temp;
 
+                        // using tuples
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+
+            }
+
+        }
+
+        public static void BubbleSort<T>(T[] array) where T : IComparable<T>
+        {
+
+        }
     }
 }
--- DataStructuresAndAlgosCSharp/Program.cs
+++ DataStructuresAndAlgosCSharp/Program.cs
@@ -17,6 +17,16 @@
             // Print the sorted array
             Console.WriteLine("Sorted array:");
             PrintArray(testArray);
+
+            // Sort using bubble sort
+            int[] testArrayBubble = { 5, 2, 9, 1, 5, 6 };
+
+            Console.WriteLine("Original array:");
+            PrintArray(testArrayBubble);
+
+            Console.WriteLine("Bubble sorted arry:");
+            BubbleSort_PN.BubbleSort(testArrayBubble);
+            PrintArray(testArrayBubble);
         }
 
         // Helper method to print the array

b276790fc38522fe87eca9bad7482a89d9a09581
Patnor <patnor22@gmail.com>
2024-09-29 10:30:01 -0700

    set up solution to implement Merge Sort.

---
+++ DataStructuresAndAlgosCSharp/MergeSort_PN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgosCSharp
+{
+    public class MergeSort_PN
+    {
+        public static void MergeSort(int[] array)
+        {
+
+        }
+
+        public static void MergeSort<T>(T[] array) where T : IComparable<T>
+        {
+
+        }
+
+
+    }
+}

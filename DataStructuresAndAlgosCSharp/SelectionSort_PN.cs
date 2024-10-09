e068b3d737eacf28e3d01cfe5e9ce6874f1fb50a
Patnor <patnor22@gmail.com>
2024-10-08 22:57:33 -0700

    commented Selection sort

--- DataStructuresAndAlgosCSharp/SelectionSort_PN.cs
+++ DataStructuresAndAlgosCSharp/SelectionSort_PN.cs
@@ -7,22 +7,31 @@ using System.Threading.Tasks;
 namespace DataStructuresAndAlgosCSharp
 {
     // n - 1 passes
+    /// <summary>
+    /// This class provides static methods to perform selection sort on arrays.
+    /// </summary>
     class SelectionSort_PN
     {
+        /// <summary>
+        /// Sorts an array of integers using the selection sort algorithm.
+        /// </summary>
+        /// <param name="a">The array of integers to be sorted.</param>
         public static void SelectionSort(int[] a)
         {
-            for(int i = 0; i < a.Length - 1; i++)
+            for (int i = 0; i < a.Length - 1; i++)
             {
                 int min = i;
-                
-                for(int j = i + 1; j < a.Length; j++)
+
+                // Find the index of the minimum element in the unsorted part of the array
+                for (int j = i + 1; j < a.Length; j++)
                 {
                     if (a[j] < a[min])
                     {
                         min = j;
                     }
-
                 }
+
+                // Swap the found minimum element with the first element of the unsorted part
                 if (min != i)
                 {
                     (a[i], a[min]) = (a[min], a[i]);
@@ -30,13 +39,20 @@ namespace DataStructuresAndAlgosCSharp
             }
         }
 
+        /// <summary>
+        /// Sorts an array of elements of type T using the selection sort algorithm.
+        /// The type T must implement the IComparable<T> interface.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the array. Must implement IComparable<T>.</typeparam>
+        /// <param name="a">The array of elements to be sorted.</param>
         public static void SelectionSort<T>(T[] a) where T : IComparable<T>
         {
-           for(int i = 0; i < a.Length - 1; i++)
+            for (int i = 0; i < a.Length - 1; i++)
             {
                 int min = i;
 
-                for(int j = i + 1; j < a.Length; j++)
+                // Find the index of the minimum element in the unsorted part of the array
+                for (int j = i + 1; j < a.Length; j++)
                 {
                     if (a[j].CompareTo(a[min]) < 0)
                     {
@@ -44,7 +60,8 @@ namespace DataStructuresAndAlgosCSharp
                     }
                 }
 
-                if(min != i)
+                // Swap the found minimum element with the first element of the unsorted part
+                if (min != i)
                 {
                     (a[i], a[min]) = (a[min], a[i]);
                 }
